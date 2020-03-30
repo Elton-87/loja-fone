350bcda8f5e27636b58304fe4e266ad1568800dd
erdac <tonrc87@gmail.com>
2020-03-30 15:36:38 -0300

    014-04 Adicionando a soma

--- js/home.js
+++ js/home.js
@@ -1,9 +1,15 @@
+let valorInicial = 10;
+
 const $second = document.querySelector(".-second");
 
 $second.addEventListener("click", handleClick);
 
 function handleClick() {
     const $carrinho = document.querySelector(".-last");
+
+    // valorInicial = valorInicial +1;
+    // valorInicial += 1;
+    // valorInicial++;
      
-    $carrinho.textContent = "Mudando o valor";
+    $carrinho.textContent = ++valorInicial;
 }
