d6616d01dc3e488f749ed3e1037bd028787dd91c
erdac <tonrc87@gmail.com>
2020-03-30 16:04:02 -0300

    014-08 Mudando para classList.toggle

--- js/description-product.js
+++ js/description-product.js
@@ -3,10 +3,6 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick);
 
 function handleClick() {
-    if ($heart.classList.contains("-active")) {
-        $heart.classList.remove("-active"); 
-    } else {
-        $heart.classList.add("-active");
-    }
+    $heart.classList.toggle("-active");
       
 }
