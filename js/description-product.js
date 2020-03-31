4de0127ef298a32903182074c1b2314571e11133
erdac <tonrc87@gmail.com>
2020-03-31 15:33:19 -0300

    015-03 Alterando o click da estrela

--- js/description-product.js
+++ js/description-product.js
@@ -1,8 +1,15 @@
-const $heart = window.document.querySelector(".-heart");
+const $heart = document.querySelector(".-heart");
+const $firstStar = document.querySelector(".star");
 
-$heart.addEventListener("click", handleClick);
 
-function handleClick() {
+$heart.addEventListener("click", handleClickHeart);
+$firstStar.addEventListener("click", handleClickStar);
+
+function handleClickHeart() {
     $heart.classList.toggle("-active");
       
+}
+
+function handleClickStar() {
+    $firstStar.classList.toggle("-active");
 }
